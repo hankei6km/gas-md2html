32725a8e826d2417bd5823dc7a3593c22a8a2e5e
hankei6km <gh-hankei6km@outlook.jp>
2022-01-20 17:09:49 +0900

    Typo

--- scripts/clasp-with-auth.sh
+++ scripts/clasp-with-auth.sh
@@ -4,7 +4,7 @@ set -e
 CLASPRC_JSON="${HOME}/.clasprc_tmp.json"
 
 if test -f "${CLASPRC_JSON}"; then
-  echo "push with ${CLASPRC_JSON}"
+  echo "run with ${CLASPRC_JSON}"
   clasp --auth "${CLASPRC_JSON}" "${@}"
 else
   clasp "${@}"
