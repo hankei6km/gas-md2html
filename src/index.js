20ef84f43fbd8ec6930ae9d17b424689da2eeadd
hankei6km <gh-hankei6km@outlook.jp>
2022-01-21 15:50:17 +0900

    Move licens banner into top of source file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
-/**
- * gas-md2html
- * @author hankei6km
- * @copyright (c) 2022 hankei6km
- * @license MIT
- * see "LICENSE.txt" "OPEN_SOURCE_LICENSES.txt" of "md2html.zip" in
- * releases(https://github.com/hankei6km/gas-md2html/releases)
- */
-
 const _toHtml = _entry_point.toHtml;
 
 /**
