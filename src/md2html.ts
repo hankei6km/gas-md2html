1c9649392c37ff562196df792a3afe3e5ef58344
hankei6km <gh-hankei6km@outlook.jp>
2022-01-21 18:02:06 +0900

    Use "hast-util-sanitize"

--- src/md2html.ts
+++ src/md2html.ts
@@ -1,12 +1,13 @@
 import { fromMarkdown } from "mdast-util-from-markdown";
 import { toHast } from "mdast-util-to-hast";
+import { sanitize } from "hast-util-sanitize";
 import { toHtml } from "hast-util-to-html";
 
 export function md2html(md: string) {
   const mdast = fromMarkdown(md);
   const hast = toHast(mdast);
   if (hast) {
-    return toHtml(hast);
+    return toHtml(sanitize(hast));
   }
   return "";
 }
