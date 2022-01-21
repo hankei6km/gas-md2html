0344bb0c0bb4f73a757c2abf0bbb00aaa5d7d4a1
hankei6km <gh-hankei6km@outlook.jp>
2022-01-21 14:43:40 +0900

    Generate "OPEN_SOURCE_LICENSES.txt"

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from "@rollup/plugin-typescript";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import license from "rollup-plugin-license";
+import * as path from "path";
 
 const extensions = [".ts", ".js"];
 
@@ -17,5 +19,30 @@ export default {
       extensions,
     }),
     commonjs({}),
+    license({
+      sourcemap: false,
+      // cwd: ".", // Default is process.cwd()
+
+      // banner: {
+      //   commentStyle: "regular", // The default
+
+      //   content: {
+      //     file: path.join(__dirname, "LICENSE"),
+      //     encoding: "utf-8", // Default is utf-8
+      //   },
+
+      //   data() {
+      //     return {};
+      //   },
+      // },
+
+      thirdParty: {
+        // includePrivate: true, // Default is false.
+        output: {
+          file: path.join(__dirname, "build", "OPEN_SOURCE_LICENSES.txt"),
+          encoding: "utf-8", // Default is utf-8.
+        },
+      },
+    }),
   ],
 };
