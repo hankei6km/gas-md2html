4c3d1d2432b764b2850f3df546858770fc08cb68
hankei6km <gh-hankei6km@outlook.jp>
2022-01-24 13:27:58 +0900

    Make "toHtml_unsafe"

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const _toHtml = _entry_point.toHtml;
+const _toHtml_unsafe = _entry_point.toHtml_unsafe;
 
 /**
  * Mardkdown を HTML へ変換.
@@ -8,6 +9,16 @@ const _toHtml = _entry_point.toHtml;
  */
 function toHtml(md) {
   return _toHtml(md);
+}
+
+/**
+ * Mardkdown を HTML へ変換(sanitize 無し).
+ *
+ * @param {string} md
+ * @returns {string}
+ */
+function toHtml_unsafe(md) {
+  return _toHtml_unsafe(md);
 }
 
 delete _entry_point;
--- test/md2html.spec.ts
+++ test/md2html.spec.ts
@@ -1,4 +1,4 @@
-import { md2html } from "../src/md2html";
+import { md2html, md2html_unsafe } from "../src/md2html";
 
 describe("md2html()", () => {
   it("should return html", () => {
@@ -9,15 +9,48 @@ test test
     ).toEqual(`<h1>test1</h1>
 <p>test test</p>`);
   });
+
+  it("should return blank", () => {
+    expect(md2html(``)).toEqual(``);
+  });
+
   it("should sanitize html", () => {
     expect(
       md2html(`# test1
 
-<script>alsert("danger")</script>
+<div>
+<p>paragraph</p>
+<script>alert("danger")</script>
+</div>
 
 test test
 `)
     ).toEqual(`<h1>test1</h1>
+<div>
+<p>paragraph</p>
+
+</div>
+<p>test test</p>`);
+  });
+});
+
+describe("md2html_unsafe()", () => {
+  it("should not sanitize html", () => {
+    expect(
+      md2html_unsafe(`# test1
+
+<div>
+<p>paragraph</p>
+<script>alert("danger")</script>
+</div>
+
+test test
+`)
+    ).toEqual(`<h1>test1</h1>
+<div>
+<p>paragraph</p>
+<script>alert("danger")</script>
+</div>
 <p>test test</p>`);
   });
 });
