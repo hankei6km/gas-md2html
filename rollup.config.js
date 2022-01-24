773095f906642aa6e059b2ac18492ac8c77e3cd2
hankei6km <gh-hankei6km@outlook.jp>
2022-01-25 01:32:08 +0900

    Add ".prettierrc"

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,22 @@
-import typescript from "@rollup/plugin-typescript";
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import license from "rollup-plugin-license";
-import * as path from "path";
+import typescript from '@rollup/plugin-typescript'
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import license from 'rollup-plugin-license'
+import * as path from 'path'
 
-const extensions = [".ts", ".js"];
+const extensions = ['.ts', '.js']
 
 export default {
-  input: "./src/main.ts",
+  input: './src/main.ts',
   output: {
-    dir: "build",
-    format: "umd",
-    name: "_entry_point",
+    dir: 'build',
+    format: 'umd',
+    name: '_entry_point'
   },
   plugins: [
     typescript(),
     nodeResolve({
-      extensions,
+      extensions
     }),
     commonjs({}),
     license({
@@ -39,10 +39,10 @@ export default {
       thirdParty: {
         // includePrivate: true, // Default is false.
         output: {
-          file: path.join(__dirname, "build", "OPEN_SOURCE_LICENSES.txt"),
-          encoding: "utf-8", // Default is utf-8.
-        },
-      },
-    }),
-  ],
-};
+          file: path.join(__dirname, 'build', 'OPEN_SOURCE_LICENSES.txt'),
+          encoding: 'utf-8' // Default is utf-8.
+        }
+      }
+    })
+  ]
+}
--- test/md2html.spec.ts
+++ test/md2html.spec.ts
@@ -1,20 +1,20 @@
-import { md2html, md2html_unsafe } from "../src/md2html";
+import { md2html, md2html_unsafe } from '../src/md2html'
 
-describe("md2html()", () => {
-  it("should return html", () => {
+describe('md2html()', () => {
+  it('should return html', () => {
     expect(
       md2html(`# test1
 test test
 `)
     ).toEqual(`<h1>test1</h1>
-<p>test test</p>`);
-  });
+<p>test test</p>`)
+  })
 
-  it("should return blank", () => {
-    expect(md2html(``)).toEqual(``);
-  });
+  it('should return blank', () => {
+    expect(md2html(``)).toEqual(``)
+  })
 
-  it("should sanitize html", () => {
+  it('should sanitize html', () => {
     expect(
       md2html(`# test1
 
@@ -30,12 +30,12 @@ test test
 <p>paragraph</p>
 
 </div>
-<p>test test</p>`);
-  });
-});
+<p>test test</p>`)
+  })
+})
 
-describe("md2html_unsafe()", () => {
-  it("should not sanitize html", () => {
+describe('md2html_unsafe()', () => {
+  it('should not sanitize html', () => {
     expect(
       md2html_unsafe(`# test1
 
@@ -51,6 +51,6 @@ test test
 <p>paragraph</p>
 <script>alert("danger")</script>
 </div>
-<p>test test</p>`);
-  });
-});
+<p>test test</p>`)
+  })
+})
