f8a5990ba68a91e79e7d8ddd08e7ab4f95fa4e0c
hankei6km <gh-hankei6km@outlook.jp>
2022-01-25 22:22:24 +0900

    Add multiple cell testing for built code

--- test/build/md2html_src.js
+++ test/build/md2html_src.js
@@ -11,6 +11,11 @@ test test
 
 <p>test test</p>`)
   })
+
+  it('should joined html', () => {
+    expect(toHtml([['# test1', 'paragraph']])).toEqual(`<h1>test1</h1>
+<p>paragraph</p>`)
+  })
 })
 
 describe('toHtml_unsafe()', () => {
@@ -25,5 +30,10 @@ test test
     ).toEqual(`<h1>test1</h1>
 <script>alert('danger')</script>
 <p>test test</p>`)
+  })
+
+  it('should joined html', () => {
+    expect(toHtml_unsafe([['# test1', 'paragraph']])).toEqual(`<h1>test1</h1>
+<p>paragraph</p>`)
   })
 })
