68ed932e1f36dacf2df52345b19c4cd325c0cd97
hankei6km <gh-hankei6km@outlook.jp>
2022-01-28 01:24:05 +0900

    Make "index.d.ts"("index.js" から強引に namespace 付き定義作成)

--- scripts/test-build.sh
+++ scripts/test-build.sh
@@ -1,6 +1,8 @@
 #!/bin\bash
 set -e
 
+BASENAME="${npm_package_config_basename}"
+
 # ビルドされたコードにテスト用のコードを結合する.
 # ビルドされたコードはエクスポートされていないための対応.
-cat test/build/md2html_src.js build/md2html.js > test/build/md2html.spec.js
+cat test/build/md2html_src.js "build/${BASENAME}".js > "test/build/${BASENAME}.spec.js"
